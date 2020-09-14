5494bcb3fa6f51126fcb2bf00ed945371cd82e57
Dhiraj Gandhi <g.prakashchand@gmail.com>
2020-09-14 13:33:18 -0400

    minor change for python3

--- robots/LoCoBot/locobot_calibration/scripts/solve_for_calibration_params.py
+++ robots/LoCoBot/locobot_calibration/scripts/solve_for_calibration_params.py
@@ -58,7 +58,8 @@ def load_data(dir_name):
     :return: data from states.pkl file in directory, images from directory
     :rtype: dict, list of cv2 images (numpy arrays)
     """
-    tt = pickle.load(open(os.path.join(dir_name, 'states.pkl')))
+    with open(os.path.join(dir_name, "states.pkl"), "rb") as f:
+        tt = pickle.load(f)
     imgs = []
     for i in range(tt['states'].shape[0]):
         img = cv2.imread(os.path.join(
